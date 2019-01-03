3708b965e8b5197922f84977e2e36e62613c032d
Heshanka <heshanka123@gmail.com>
2019-01-04 02:23:18 +0530

    Rectangle with Hello text

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HelloRectangle extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Container(
+        color: Colors.blueGrey,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text("Hello!",
+            style: TextStyle(fontSize: 40.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Rectangle"),
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
+}
